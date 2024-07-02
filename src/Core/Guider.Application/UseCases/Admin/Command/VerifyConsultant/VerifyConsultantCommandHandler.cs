250478fddbacfba9e2d9decd2d5e813d6409a092
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-07-02 05:09:47 +0300

    use mail service in verify handler

--- src/Core/Guider.Application/UseCases/Admin/Command/VerifyConsultant/VerifyConsultantCommandHandler.cs
+++ src/Core/Guider.Application/UseCases/Admin/Command/VerifyConsultant/VerifyConsultantCommandHandler.cs
@@ -1,5 +1,8 @@
-﻿using Guider.Application.Contracts.Persistence;
+﻿using Guider.Application.Contracts.Infrastructure;
+using Guider.Application.Contracts.Persistence;
 using Guider.Application.Responses;
+using Guider.Domain.Entities;
+using Guider.Domain.Enums;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,10 +16,16 @@ namespace Guider.Application.UseCases.Admin.Command.VerifyConsultant
     public class VerifyConsultantCommandHandler : IRequestHandler<VerifyConsultantCommand, BaseResponse>
     {
         private readonly IConsultantRepository _repository;
+        private readonly IRepository<User> _userRepository;
+        private readonly IMailFactory _mailFactory;
+        private readonly IMailService _mailService;
 
-        public VerifyConsultantCommandHandler(IConsultantRepository repository)
+        public VerifyConsultantCommandHandler(IConsultantRepository repository, IMailFactory mailFactory, IMailService mailService, IRepository<User> userRepository)
         {
             _repository = repository;
+            _mailFactory = mailFactory;
+            _mailService = mailService;
+            _userRepository = userRepository;
         }
 
         public async Task<BaseResponse> Handle(VerifyConsultantCommand request, CancellationToken cancellationToken)
@@ -30,6 +39,13 @@ namespace Guider.Application.UseCases.Admin.Command.VerifyConsultant
             var res = await _repository.UpdateAsync(con);
             if(!res)
                 throw new Exceptions.BadRequestException("Can not Verified Consultant");
+
+            var user = await _userRepository.GetByIdAsync(con.UserId);
+            if (user == null)
+                throw new Exceptions.BadRequestException("Can not Verified Consultant");
+
+            var mes = _mailFactory.GenerateMailMssage(MailType.ConfirmConsultant, user);
+            await _mailService.SendMailAsync(mes);
 
             return new BaseResponse { Success = true };
 
--- src/Infrastructure/Guider.Infrastructure/Mail/MailFactory.cs
+++ src/Infrastructure/Guider.Infrastructure/Mail/MailFactory.cs
@@ -15,11 +15,12 @@ namespace Guider.Infrastructure.Mail
     {
         public MailMessage GenerateMailMssage(MailType mailType, User user)
         {
+            var confBody = $"Dear {user.FirstName},\r\n\r\nCongratulations! Your Guider profile has been verified. You can now offer your expertise and manage your availability for consultations.";
             var mes = new MailMessage();
             switch(mailType)
             {
                 case MailType.ConfirmConsultant:
-                    return new MailMessage(from:"admin@guider.com",to:user.Email!,"Confirm Consultant Profile", "Your Profil Was Verified");
+                    return new MailMessage(from:"admin@guider.com",to:user.Email!, "Your Guider Profile Has Been Verified!", confBody);
                 case MailType.MakeAppointment:
                     return new MailMessage(from: "admin@guider.com", to: user.Email!, "Session Appointment", $"Hello {user.FirstName}, you have a new Appointment.");
                 case MailType.Register:
