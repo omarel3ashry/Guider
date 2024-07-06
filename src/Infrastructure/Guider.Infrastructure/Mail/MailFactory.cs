126a4d94f224dde8c08a46b3498564a6948356bf
Mohamed Abdelrahman <mohamedbdelrahman2@gmail.com>
2024-07-06 19:14:57 +0300

    refactor mail factory method

--- src/Infrastructure/Guider.Infrastructure/Mail/MailFactory.cs
+++ src/Infrastructure/Guider.Infrastructure/Mail/MailFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Guider.Application.Contracts.Infrastructure;
 using Guider.Domain.Entities;
 using Guider.Domain.Enums;
+using Guider.Infrastructure.Mail.Strategies;
 using System.Net.Mail;
 
 namespace Guider.Infrastructure.Mail
@@ -9,19 +10,14 @@ namespace Guider.Infrastructure.Mail
     {
         public MailMessage GenerateMailMssage(MailType mailType, User user)
         {
-            string body = $"Dear {user.FirstName},\r\n\r\nCongratulations! Your Guider profile has been verified. You can now offer your expertise and manage your availability for consultations.";
-
-            switch (mailType)
+            IMailStrategy mailStrategy = mailType switch
             {
-                case MailType.ConfirmConsultant:
-                    return new MailMessage(from: "mohamedbdelrahman2@gmail.com", to: user.Email!, "Your Guider Profile Has Been Verified!", body);
-                case MailType.MakeAppointment:
-                    return new MailMessage(from: "mohamedbdelrahman2@gmail.com", to: user.Email!, "Session Appointment", $"Hello {user.FirstName}, you have a new Appointment.");
-                case MailType.Register:
-                    return new MailMessage(from: "mohamedbdelrahman2@gmail.com", to: user.Email!, "Successful Registeration", $"Hello {user.FirstName}! ");
-                default:
-                    return new MailMessage(from: "mohamedbdelrahman2@gmail.com", to: user.Email!, "Hello!", "Hello in Our Family");
-            }
+                MailType.Register => new RegisterMailStrategy(user),
+                MailType.ConfirmConsultant => new ConfirmConsultantMailStrategy(user),
+                MailType.MakeAppointment => new MakeAppointmentMailStrategy(user),
+                _ => new DefaultMailStrategy(user),
+            };
+            return mailStrategy.GetMailMessage();
         }
     }
 }
--- src/Infrastructure/Guider.Infrastructure/Mail/Strategies/ConfirmConsultantMailStrategy.cs
+++ src/Infrastructure/Guider.Infrastructure/Mail/Strategies/ConfirmConsultantMailStrategy.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Guider.Application.Contracts.Infrastructure;
+using Guider.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Guider.Infrastructure.Mail.Strategies
 {
-    internal class ConfirmConsultantMailStrategy
+    public class ConfirmConsultantMailStrategy : IMailStrategy
     {
+        private readonly string fromEmail;
+        private readonly string toEmail;
+        private readonly string subject;
+        private readonly string body;
+
+        public ConfirmConsultantMailStrategy(User user)
+        {
+            fromEmail = "mohamedbdelrahman2@gmail.com";
+            toEmail = user.Email!;
+            subject = "Your Guider Profile Has Been Verified!";
+            body = $"Dear {user.FirstName},\r\n\r\nCongratulations! Your Guider profile has been verified." +
+                " You can now offer your expertise and manage your availability for consultations.";
+        }
+        public MailMessage GetMailMessage()
+        {
+            return new MailMessage(from: fromEmail, to: toEmail, subject, body);
+        }
     }
 }
